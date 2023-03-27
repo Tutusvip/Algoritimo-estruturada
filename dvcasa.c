97290a6aa8334e55515479426ec8a9f3a7623143
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2023-03-27 11:23:09 -0300

    Add files via upload

---
+++ dvcasa.c
@@ -0,0 +1,19 @@
+//faça o valor de 5 vendas e mostre como resultado A quantas vendas foram maiors que 500 letra B qual a media Obs: no maximo 4 variavies e não usar vetor
+#include <stdlib.h>
+#include "conio2.h"
+
+
+main(){
+	int cont,valor,M,media;
+	for(cont=0;cont<4;cont++){
+	printf("Digite o numero de vendas:");
+	scanf("%d", &valor);
+if(valor>500){
+	
+}if(){
+
+}
+}
+printf("O valor maior %d\n", M);
+printf("O valor medio foi %d", media);
+}
