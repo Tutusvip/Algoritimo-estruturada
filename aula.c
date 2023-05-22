4d7cd2885d2ba50c618729fb57e3d21be8cd271d
tutusvip <111453250+Tutusvip@users.noreply.github.com>
2023-05-22 11:12:56 -0300

    Update aula.c

--- aula.c
+++ aula.c
@@ -1,62 +1,74 @@
-[Project]
-FileName=aula.c
-Name=cafe
-Type=1
-Ver=2
-ObjFiles=
-Includes=
-Libs=
-PrivateResource=
-ResourceIncludes=
-MakeIncludes=
-Compiler=
-CppCompiler=
-Linker=
-IsCpp=0
-Icon=
-ExeOutput=
-ObjectOutput=
-LogOutput=
-LogOutputEnabled=0
-OverrideOutput=0
-OverrideOutputName=
-HostApplication=
-UseCustomMakefile=0
-CustomMakefile=
-CommandLine=
-Folders=
-IncludeVersionInfo=0
-SupportXPThemes=0
-CompilerSet=0
-CompilerSettings=0000000000000000000000000
-UnitCount=1
+/* cod1)#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+ 
 
-[VersionInfo]
-Major=1
-Minor=0
-Release=0
-Build=0
-LanguageID=1033
-CharsetID=1252
-CompanyName=
-FileVersion=
-FileDescription=Developed using the Dev-C++ IDE
-InternalName=
-LegalCopyright=
-LegalTrademarks=
-OriginalFilename=
-ProductName=
-ProductVersion=
-AutoIncBuildNr=0
-SyncProduct=1
+main(){
+	system("color 5e");
+	int k;
+	setlocale(LC_ALL, "Portuguese");
+	printf("Digite um número: ");
+	scanf("%d", &k);
+	printf("Valor digitado %d \n", k);
+	printf("O endereço de k = %x ", &k);
+	
+}
 
-[Unit1]
-FileName=main.c
-CompileCpp=0
-Folder=
-Compile=1
-Link=1
-Priority=1000
-OverrideBuildCmd=0
-BuildCmd=
+cod2)#include <stdio.h>
+#include <locale.h>
+int main(){
+	system("color 5e");
+	setlocale(LC_ALL, "Portuguese");
+	char matriz[10] = "palavra";
+	
+	printf("Tamanho de um int short é: %d bytes\n", sizeof(short int));
+	printf("Tamanho de um int long é: %d bytes\n", sizeof(long int));
+	printf("Tamanho de um char é: %d bytes\n", sizeof(char));
+	printf("Tamanho de um vetor de char com 10 posições é: %d bytes\n", sizeof(matriz));
+}
 
+cod3)#include <stdio.h>
+
+int main(){
+char letra='E';
+int numero=35;
+char matriz[20]="estruturaDeDados";
+float numeroDecimal = 87.8;
+printf("Endereço de memória das variáveis\n\n");
+printf("O valor da variável letra é %c e seu endereço é %x\n",letra,&letra);
+printf("O valor da variável numero é %d e seu endereço é %x\n",numero,&numero);
+printf("O valor da variável metriz é %s e seu endereço é %x\n",matriz,matriz);
+printf("O valor da variável numeroDecimal é %2.1f e seu endereço é %x\n",numeroDecimal,&numeroDecimal);
+
+
+
+faça um programa que receba como dados de 2 numeros e execute as 4 operações aritimiceitcas usando ponteiroes*/
+
+
+cod4) #include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main(){
+	system("color 5e");
+	setlocale(LC_ALL, "Portuguese");
+	int n1, n2;
+	int *p1,*p2;
+	
+	printf("Digite os Dados");
+	scanf("%d",&n1);
+	printf("Digite os Dados");
+	scanf("%d",&n2);
+	
+	p1=&n1;
+	p2=&n2;
+
+
+
+	printf("subi = %d\n", *p1 - * p2);
+	printf("soma = %d\n", *p1 + * p2);
+	printf("Divi = %d\n", *p1 / * p2);
+	printf("Mult = %d\n", (*p1) * (*p2));
+		
+	return(0);
+}
